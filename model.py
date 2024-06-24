f92fa60e0877e0cb877461dff20ce52950a6547b
omshetye <103515277+omshetye@users.noreply.github.com>
2024-06-24 22:14:05 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class Model(nn.Module):
+class FFNN(nn.Module):
     def __init__(self, in_size, hidden_size1, hidden_size2, hidden_size3, out_size):
         super().__init__()
         # hidden layer 1
@@ -37,3 +37,40 @@ class Model(nn.Module):
         yb = self(xb)
         conf, preds  = torch.max(yb, dim=1)
         return conf, preds[0].item()
+    
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.network = nn.Sequential(
+            # size: 1 x 28 x 28
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            # size: 32 x 28 x 28
+            nn.Conv2d(32, 64, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # size: 64 x 14 x 14
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            # size: 128 x 14 x 14
+            nn.Conv2d(128, 256, kernel_size=3, padding=1, stride=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # size: 256 x 7 x 7
+            nn.Flatten(),
+            nn.Linear(256*7*7, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Linear(512, 10))
+        
+    def forward(self, xb):
+        return self.network(xb)
+
+    def predict(self, img):
+        xb = img.unsqueeze(0)
+        yb = self(xb)
+        conf, preds  = torch.max(yb, dim=1)
+        return conf, preds[0].item() 
